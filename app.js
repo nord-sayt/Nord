084d230320bca8595e123a535671129bf9cd4b59
mashslash <1292mohammadamin1292@gmail.com>
2026-06-02 15:52:32 +0330

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function download(name){
-alert("در حال آماده‌سازی دانلود: " + name);
-}
-
-function copyIP(){
-let ip = document.getElementById("ip").innerText;
-navigator.clipboard.writeText(ip);
-alert("IP کپی شد ✔");
-}
