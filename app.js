b1e4857d0c1fb5dc5219b5eb8d9ef9a5dab23727
mashslash <1292mohammadamin1292@gmail.com>
2026-06-02 15:49:26 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("NORD site loaded");
+function download(name){
+alert("در حال آماده‌سازی دانلود: " + name);
+}
+
+function copyIP(){
+let ip = document.getElementById("ip").innerText;
+navigator.clipboard.writeText(ip);
+alert("IP کپی شد ✔");
+}
